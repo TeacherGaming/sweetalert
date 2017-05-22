3d5056843952526b08728d726980deb2564803e7
Jaradacl <rassse333@gmail.com>
2017-05-22 11:36:51 +0300

    changed package name & git repo link

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    name: 'nerdmed:sweet-alert',
-    version: '1.0.3',
+    name: 'teachergaming:sweet-alert',
+    version: '1.0.4',
 
     // Brief, one-line summary of the package.
     summary: 'Just raw sweet alert 2 lib',
 
     // URL to the Git repository containing the source code for this package.
-    git: 'git@github.com:nerdmed/sweetalert.git',
+    git: 'git@github.com:TeacherGaming/sweetalert.git',
 
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
